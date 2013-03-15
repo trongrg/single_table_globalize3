ba1040f2a165a935ad5faf9e40ff243ceecc225c
Trong Tran <trongrg@gmail.com>
2013-03-15 16:49:37 +1100

    add migrations for paper_trail

--- lib/generators/single_table_globalize3/migration_generator.rb
+++ lib/generators/single_table_globalize3/migration_generator.rb
@@ -4,9 +4,12 @@ module SingleTableGlobalize3
   class MigrationGenerator < ::ActiveRecord::Generators::Base
     source_root File.expand_path("../templates", __FILE__)
     argument :name, :type => :string, :default => "application"
+    class_option :with_versioning, :type => :boolean, :default => false, :desc => "Generate migration for versioning"
 
-    def copy_devise_migration
+    def create_migration_file
       migration_template "migration.rb", "db/migrate/create_globalize_translations"
+      generate "paper_trail:install #{"--force" if options.force?}" if options.with_versioning?
+      migration_template "add_locale_column_to_versions.rb", "db/migrate/add_locale_column_to_versions" if options.with_versioning?
     end
   end
 end
